369057de4f21f7d33f9e9d95d2312e4ea02d8a1a
AnviJoy <anvi@praveenjoy.com>
2026-07-14 18:07:31 +0200

    Update test.py

--- Proc /test.py
+++ Proc /test.py
@@ -1,1 +1,9 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import netCDF4 as nc
+from skimage.morphology import remove_small_objects, binary_closing, disk
+from scipy.ndimage import binary_fill_holes
+from netCDF4 import Dataset
+import matplotlib.colors as mcolors
